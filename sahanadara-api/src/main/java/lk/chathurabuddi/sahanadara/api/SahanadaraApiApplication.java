3ba76a9ba877f1923f198b66c9c7cc95bff61d4c
chathurabuddi <chathu.buddi@gmail.com>
2020-07-13 21:37:54 +0530

    change the packaging type to war

--- sahanadara-api/src/main/java/lk/chathurabuddi/sahanadara/api/SahanadaraApiApplication.java
+++ sahanadara-api/src/main/java/lk/chathurabuddi/sahanadara/api/SahanadaraApiApplication.java
@@ -2,12 +2,13 @@ package lk.chathurabuddi.sahanadara.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SahanadaraApiApplication {
+public class SahanadaraApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SahanadaraApiApplication.class, args);
